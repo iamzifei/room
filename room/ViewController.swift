70114a789dc8bd174114fb194b2d97cda23bdbac
CY Lim <cylim226@gmail.com>
2016-10-22 11:40:42 +1100

    Initial Commit

---
+++ room/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  room
+//
+//  Created by CY Lim on 22/10/16.
+//  Copyright © 2016 cy.my. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
